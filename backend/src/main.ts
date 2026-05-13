31b5d638573481016b73866897c3f5cdb8a8e225
Fabritcio PS <fabpsandoval@gmail.com>
2026-05-13 12:37:10 -0500

    cambio a ver

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,8 +8,16 @@ async function bootstrap() {
 
   app.enableCors({
     origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
+  });
+
+  app.use((req, res, next) => {
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
+    next();
   });
 
   // Prefijo global para todas las rutas
